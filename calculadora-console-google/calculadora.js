fb470d0a085b2240eeda51709609e7080aa094d4
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-04-10 17:46:12 -0300

    Initial commit

---
+++ calculadora-console-google/calculadora.js
@@ -0,0 +1,19 @@
+
+var numeroUm = Number(prompt('Digite o primeiro número:'))
+var numeroDois = Number(prompt('Digite o segundo número:'))
+var condicao = prompt('Escolha o tipo de operação que deseja: + - * /')
+switch(condicao){
+    case '+' :
+        console.log(`${numeroUm} + ${numeroDois} = ${numeroUm+numeroDois}`)
+        break
+    case '-' :
+        console.log(`${numeroUm} - ${numeroDois} = ${numeroUm-numeroDois}`)
+    case '*' :
+        console.log(`${numeroUm} * ${numeroDois} = ${numeroUm*numeroDois}`)
+        break
+    case '/' :
+        console.log(`${numeroUm} / ${numeroDois} = ${numeroUm/numeroDois}`)
+        break
+    default:
+    break
+}
